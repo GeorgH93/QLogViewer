0d20f11ec45b238a4b531c5fa3213d1a91185f08
GeorgH93 <git@georgh93.eu>
2023-04-25 21:16:06 +0200

    Refactor LogEntry

--- src/LogParser.cpp
+++ src/LogParser.cpp
@@ -128,26 +128,25 @@ LogEntry LogParser::ParseMessage(const QString& message, uint64_t startLineNumbe
 	TryExtractEnvironment(message);
 	if (match.hasMatch())
 	{
-		e.date = match.captured("date");
-		e.time = match.captured("time");
-		e.thread = match.captured("thread");
-		e.subSystem = match.captured("subsys");
-		e.message = match.captured("message");
-		e.where = match.captured("where");
+		e.components[LogComponent::DATE] = match.captured("date");
+		e.components[LogComponent::TIME] = match.captured("time");
+		e.components[LogComponent::THREAD] = match.captured("thread");
+		e.components[LogComponent::SUB_SYS] = match.captured("subsys");
+		e.components[LogComponent::MESSAGE] = match.captured("message");
+		e.components[LogComponent::WHERE] = match.captured("where");
 
 		// Read log level
 		e.level = GetLogLevel(match.captured("level"));
 
 		//TODO
-		e.timeStamp = QDateTime::fromString("20" + e.date + ' ' + e.time, Qt::ISODateWithMs);
+		e.timeStamp = QDateTime::fromString("20" + e.components[LogComponent::DATE] + ' ' + e.components[LogComponent::TIME], Qt::ISODateWithMs);
 	}
 	else
 	{
-		e.message = message;
+		e.components[LogComponent::MESSAGE] = message;
 		e.level = GetLogLevel("");
 	}
-	e.originalMessage = message;
-	e.Process();
+	e.components[LogComponent::ORIGINAL_MESSAGE] = message;
 
 	return e;
 }
